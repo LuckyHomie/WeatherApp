8541811e6111aee4334be8dfbef168d7988679bb
LuckyHomie <lszuligowski@gmail.com>
2022-12-05 23:45:55 +0100

    Initial commit

---
+++ WeatherApp/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WeatherApp.Models
+{
+	public class User
+	{
+		public int Id;
+		public int RoleId { get; set; }
+		public string Name { get; set; }
+		public string Login { get; set; }
+		public string Password { get; set; }
+		public string EmailAddress { get; set; }
+		[DataType(DataType.Date)]
+		public DateTime RegistrationDate { get; set; }
+	}
+}
+
