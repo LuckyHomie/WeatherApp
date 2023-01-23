fb2f656196cd0addeb9d3a50070c98925e3a93c7
LuckyHomie <lszuligowski@gmail.com>
2023-01-23 05:00:22 +0100

    Cleaning code
    Cleaning code

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WeatherApp.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
