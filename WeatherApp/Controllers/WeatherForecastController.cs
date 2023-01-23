86507289c7b9691488da255f379221305b485f80
LuckyHomie <lszuligowski@gmail.com>
2023-01-23 05:02:26 +0100

    Adding run files
    Adding run files

---
+++ WeatherApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using WeatherApp.Interfaces;
+
+namespace WeatherApp.Controllers
+{
+	[ApiController]
+	public class WeatherForecastController : ControllerBase
+	{
+		private readonly IWeatherClient _weatherClient;
+
+        public WeatherForecastController(IWeatherClient weatherClient)
+        {
+            _weatherClient = weatherClient;
+        }
+
+        [HttpGet("weather/{city}")]
+		public async Task<IActionResult> Forecast(string city)
+		{
+			var weather = await _weatherClient.GetCurrentWeatherForCity(city);
+			return weather is not null ? Ok(weather) : NotFound();
+		}
+	}
+}
+
