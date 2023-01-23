ea2e74034ca1a95906925b12b95fcc0cc69f7e59
LuckyHomie <68870079+LuckyHomie@users.noreply.github.com>
2023-01-23 04:46:47 +0100

    Add files via upload

---
+++ Clients/OpenWeatherClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using WeatherApp.Models;
+using WeatherApp.Interfaces;
+
+namespace WeatherApp.Clients
+{
+	public class OpenWeatherClient : IWeatherClient
+	{
+		private const string OpenWeatherMapApiKey = "3b0d16dab44859d1ecb30e52b478dd48";
+
+		private readonly HttpClient _httpClient;
+
+		public OpenWeatherClient(HttpClient httpClient)
+		{
+			_httpClient = httpClient;
+		}
+
+		public async Task<OpenWeatherResponse?> GetCurrentWeatherForCity(string city)
+		{
+			return
+				await _httpClient.GetFromJsonAsync<OpenWeatherResponse>(
+					$"forecast?q={city}&cnt=8&appid={OpenWeatherMapApiKey}&units=metric");
+		}
+    }
+}
+
---
+++ Helpers/MappingProfiles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using AutoMapper;
+using System.Net;
+using WeatherApp.DataTransferObjects;
+using WeatherApp.Models;
+
+namespace WeatherApp.Helpers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<OpenWeatherResponse, WeatherForecastDTO>();
+        }
+    }
+}
+
